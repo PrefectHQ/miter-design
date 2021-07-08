70f11042ec1af24457ca0336fcdb1b25baf61a57
nicholas <znicholasbrown@gmail.com>
2021-07-08 19:27:29 -0400

    Create miter mixin; declare first 6 miter cuts

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    Hello
     <Button>Hello!</Button>
   </div>
 </template>
@@ -13,13 +14,4 @@ import { Options, Vue } from 'vue-class-component'
 export default class App extends Vue {}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
