9ffa6e8c6603d6ec0a493a8f65550e3e114e797d
nicholas <znicholasbrown@gmail.com>
2021-07-08 13:23:33 -0400

    Scaffold project and directory structure

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Button>Hello!</Button>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import Button from './components/Button/Button.vue'
+
+@Options({
+  components: {
+    Button
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,9 @@
+<template>
+  <button><slot></slot></button>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+
+export default class Button extends Vue {}
+</script>
