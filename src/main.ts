0e9423524832d40dfafb8e039ac65a2090605376
nicholas <znicholasbrown@gmail.com>
2021-07-08 18:51:43 -0400

    Import components to main file and globally register them

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,6 @@ import '@/styles/main.scss'
 import App from './App.vue'
 import * as Components from '@/components'
 
-console.log(Components)
-
-// Object.keys(Components).forEach((component: string) => {
-//   console.log(component in Components)
-
-//   console.log(Components[component])
-//   // app.component(Components[component].name, Components[component])
-// })
-
 const app = createApp(App)
 
 app.component('Button', Components.Button)
