1a37353493dde0eb47a2b68dafd664318311ec73
nicholas <znicholasbrown@gmail.com>
2021-07-09 17:40:06 -0400

    Add module level shims and introduce plugin install logic

---
+++ src/framework.ts
@@ -0,0 +1,30 @@
+// Types
+import type { App } from 'vue'
+import type { Directive, Component } from 'vue'
+
+export interface MiterOptions {
+  components?: Record<string, Component>
+  directives?: Record<string, Directive>
+}
+
+export const createMiter = (
+  options: MiterOptions = {}
+): Record<string, unknown> => {
+  const install = (app: App): void => {
+    const { components = {}, directives = {} } = options
+
+    for (const key in directives) {
+      const directive = directives[key]
+
+      app.directive(key, directive)
+    }
+
+    for (const key in components) {
+      const component = components[key]
+
+      app.component(key, component)
+    }
+  }
+
+  return { install }
+}
