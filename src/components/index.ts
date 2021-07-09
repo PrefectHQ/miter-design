e6807552652fe21b5028e6ed499defc917427fbd
nicholas <znicholasbrown@gmail.com>
2021-07-09 15:01:45 -0400

    Import global styles to package entry point

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
+// I think we need to import global styles to make sure they're available to the packaged module
+import '@/styles/main.scss'
+
 export { default as Button } from './Button/Button.vue'
 export { default as Card } from './Card/Card.vue'
 export { default as CardActions } from './Card/Actions/Actions.vue'
