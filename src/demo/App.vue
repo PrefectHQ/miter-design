1e21b5ca93cbcf6ea97cd825901c1eb9454b218d
nicholas <znicholasbrown@gmail.com>
2021-07-19 21:32:12 -0400

    Remove unused demo stuff

--- src/demo/App.vue
+++ src/demo/App.vue
@@ -3,7 +3,6 @@
     <Cards />
     <Buttons class="mt-10" />
     <Links class="mt-10" />
-    <Tabs class="mt-10" />
   </div>
 </template>
 
@@ -12,14 +11,12 @@ import { Options, Vue } from 'vue-class-component'
 import Buttons from './sections/Buttons.vue'
 import Cards from './sections/Cards.vue'
 import Links from './sections/Links.vue'
-import Tabs from './sections/Tabs.vue'
 
 @Options({
   components: {
     Buttons,
     Cards,
-    Links,
-    Tabs
+    Links
   }
 })
 export default class App extends Vue {}
