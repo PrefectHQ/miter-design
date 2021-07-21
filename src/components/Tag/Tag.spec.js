e695fcd11e7315db5c7bca95f790541273944889
Natalie Smith <thatgalnatalie@gmail.com>
2021-07-21 12:55:41 -0700

    add hover and selected tests

--- src/components/Tag/Tag.spec.js
+++ src/components/Tag/Tag.spec.js
@@ -35,7 +35,56 @@ describe('color prop', () => {
   })
 })
 
-// Add Icon Test Here
+describe('disabled state', () => {
+  test('adds the disabled class when disabled:true is passed as a prop', () => {
+    const wrapper = mount(Tag, {
+      props: {
+        disabled: true
+      }
+    })
+
+    const tag = wrapper.get('.tag')
+
+    expect(tag.classes()).toContain('disabled')
+  })
+
+  test('adds the disabled attribute when disabled:true is passed as a prop', () => {
+    const wrapper = mount(Tag, {
+      props: {
+        disabled: true
+      }
+    })
+
+    const tag = wrapper.get('.tag')
+
+    expect(tag.attributes('disabled')).toBeDefined()
+  })
+
+  test("doesn't pass the disabled attribute when disabled:false is passed as a prop", () => {
+    const wrapper = mount(Tag, {
+      props: {
+        disabled: false
+      }
+    })
+
+    const tag = wrapper.get('.tag')
+
+    expect(tag.attributes('disabled')).toBeUndefined()
+  })
+})
+
+describe('hovered state', () => {
+  const wrapper = mount(Tag, {})
+  const tag = wrapper.get('.tag')
+
+  test('mouseenter adds the hovered class and mouseleave removes the hovered class', async () => {
+    await tag.trigger('mouseenter')
+    expect(tag.classes()).toContain('hovered')
+
+    await tag.trigger('mouseleave')
+    expect(tag.classes()).not.toContain('hovered')
+  })
+})
 
 describe('disabled state', () => {
   test('adds the disabled class when disabled:true is passed as a prop', () => {
@@ -75,8 +124,28 @@ describe('disabled state', () => {
   })
 })
 
-// Add Hover States
+describe('selected state', () => {
+  test('adds the selected class when selected:true is passed as a prop', () => {
+    const wrapper = mount(Tag, {
+      props: {
+        selected: true
+      }
+    })
 
-// Selected States
+    const tag = wrapper.get('.tag')
 
-// Active States
+    expect(tag.classes()).toContain('selected')
+  })
+
+  test("doesn't pass the selected attribute when selected:false is passed as a prop", () => {
+    const wrapper = mount(Tag, {
+      props: {
+        selected: false
+      }
+    })
+
+    const tag = wrapper.get('.tag')
+
+    expect(tag.attributes('selected')).toBeUndefined()
+  })
+})
