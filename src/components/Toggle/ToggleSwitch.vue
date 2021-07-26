5cb10cc27d357f046d301250390685efedd5cc8e
Allyson Lubimir <allyson@prefect.io>
2021-07-26 09:49:27 -0400

    init

---
+++ src/components/Toggle/ToggleSwitch.vue
@@ -0,0 +1,95 @@
+<template>
+  <label
+    class="toggle ml-2"
+    :class="classList"
+    :disabled="disabled"
+    @mouseenter="handleMouseEnter"
+    @mouseleave="handleMouseLeave"
+    @mousedown="handleMouseDown"
+    @mouseup="handleMouseUp"
+  >
+    <input
+      type="checkbox"
+      :disabled="disabled"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.checked)"
+      @focus="handleFocus"
+      @blur="handleBlur"
+    />
+    <span class="toggle-switch"></span>
+    <span data-test="default"><slot /></span>
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ToggleSwitch',
+  emits: {
+    'update:modelValue'(...args: any[]) {
+      return { ...args }
+    }
+  },
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    modelValue: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      hovered: false,
+      active: false
+    }
+  },
+  computed: {
+    classList(): string[] {
+      return [
+        ...(this.disabled ? ['disabled'] : []),
+        ...(this.hovered ? ['hovered'] : []),
+        ...(this.active ? ['hovered'] : [])
+      ]
+    }
+  },
+  methods: {
+    handleMouseEnter(): void {
+      if (this.disabled) return
+      this.hovered = true
+    },
+
+    handleMouseLeave(): void {
+      this.hovered = false
+      this.active = false
+    },
+
+    handleMouseDown(): void {
+      if (this.disabled) return
+      this.active = true
+    },
+
+    handleMouseUp(): void {
+      if (this.disabled) return
+      this.active = false
+    },
+
+    handleFocus(): void {
+      if (this.disabled) return
+      this.hovered = true
+    },
+
+    handleBlur(): void {
+      this.hovered = false
+      this.active = false
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@use '../../styles/components/toggle';
+</style>
