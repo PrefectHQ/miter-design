8fe5f1fa6f70508e23237e8064736dc5d05bf6f7
Natalie Smith <thatgalnatalie@gmail.com>
2021-07-27 13:28:26 -0700

    add in icons in demo

--- src/demo/sections/Avatar.vue
+++ src/demo/sections/Avatar.vue
@@ -8,7 +8,7 @@
       class="my-2 ml-1"
     >
       <Avatar :color="style">
-        <span v-if="backgroundColors[3] !== style"> NS</span>
+        <span v-if="backgroundColors[3] !== style"> TS</span>
         <span v-else>+1</span>
       </Avatar>
     </div>
@@ -22,6 +22,16 @@
       <Avatar :color="style">
         <img src="https://www.w3schools.com/howto/img_avatar.png" alt="John" />
       </Avatar>
+    </div>
+
+    <br />
+    <div
+      v-for="style in backgroundColors"
+      :key="style"
+      style="display: inline-flex"
+      class="my-2 ml-1"
+    >
+      <Avatar :color="style"> <i class="pi pi-fire pi-2x"></i> </Avatar>
     </div>
   </div>
 </template>
