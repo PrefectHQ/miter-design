90f3a5e0290a5aec9e6b402ade2975ee49743eaf
Allyson Lubimir <allyson@prefect.io>
2021-07-27 11:01:17 -0400

    standardize on wrapper.get, remove unnecessary props

--- src/components/Radio/Radio.spec.js
+++ src/components/Radio/Radio.spec.js
@@ -40,11 +40,7 @@ describe('disabled state', () => {
   })
 
   test('sets disabled to false when disabled:false is passed as a prop', () => {
-    const wrapper = mount(RadioButton, {
-      props: {
-        disabled: false
-      }
-    })
+    const wrapper = mount(RadioButton)
 
     const radio = wrapper.get('label')
 
@@ -53,13 +49,8 @@ describe('disabled state', () => {
 })
 
 test('emit update to true when selected', async () => {
-  const wrapper = mount(RadioButton, {
-    props: {
-      disabled: false,
-      modelValue: false
-    }
-  })
-  const input = wrapper.find('input[type="radio"]')
+  const wrapper = mount(RadioButton)
+  const input = wrapper.get('input[type="radio"]')
   input.element.checked = true
   await input.trigger('input')
   expect(wrapper.emitted('update:modelValue')[0][0]).toEqual(true)
@@ -68,11 +59,10 @@ test('emit update to true when selected', async () => {
 test('emit update to false when unselected', async () => {
   const wrapper = mount(RadioButton, {
     props: {
-      disabled: false,
       modelValue: true
     }
   })
-  const input = wrapper.find('input[type="radio"]')
+  const input = wrapper.get('input[type="radio"]')
   input.element.checked = false
   await input.trigger('input')
   expect(wrapper.emitted('update:modelValue')[0][0]).toEqual(false)
