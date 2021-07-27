7062db2abb1908bf814ca2b530f838c97cc79021
Natalie Smith <thatgalnatalie@gmail.com>
2021-07-27 16:18:13 -0700

    add stacked avatars to demo

--- src/demo/sections/Avatar.vue
+++ src/demo/sections/Avatar.vue
@@ -1,39 +1,50 @@
 <template>
   <div>
     <h3>Avatar</h3>
-    <div
-      v-for="style in backgroundColors"
-      :key="style"
-      style="display: inline-flex"
-      class="my-2 ml-1"
-    >
-      <Avatar :color="style">
-        <div v-if="backgroundColors[3] !== style"> TS</div>
-        <div v-else>+1</div>
+
+    <div class="avatars">
+      <Avatar :color="'success'" class="avatar">
+        <div>+1</div>
       </Avatar>
-    </div>
-    <br />
-    <div
-      v-for="style in backgroundColors"
-      :key="style"
-      style="display: inline-flex"
-      class="my-2 ml-1"
-    >
-      <Avatar :color="style">
-        <img src="https://www.w3schools.com/howto/img_avatar.png" alt="John" />
+      <Avatar class="avatar">
+        <div> A </div>
+      </Avatar>
+
+      <Avatar :color="'error'" class="avatar">
+        <div> B</div>
+      </Avatar>
+
+      <Avatar :color="'warning'" class="avatar">
+        <div>C</div>
       </Avatar>
     </div>
 
-    <br />
-    <div
-      v-for="style in backgroundColors"
-      :key="style"
-      style="display: inline-flex"
-      class="my-2 ml-1"
-    >
-      <Avatar :color="style">
-        <i class="pi pi-fire pi-2x"></i>
-      </Avatar>
+    <div style="display: flex">
+      <div
+        v-for="style in backgroundColors"
+        :key="style"
+        class="my-1 ml-1"
+        style="inline-flex"
+      >
+        <Avatar :color="style">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="John"
+          />
+        </Avatar>
+      </div>
+    </div>
+    <div style="display: flex">
+      <div
+        v-for="style in backgroundColors"
+        :key="style"
+        style="display: flex"
+        class="my-1 ml-1"
+      >
+        <Avatar :color="style">
+          <i class="pi pi-fire pi-2x"></i>
+        </Avatar>
+      </div>
     </div>
   </div>
 </template>
@@ -53,4 +64,17 @@ export default class Avatar extends Vue {
 
 <style lang="scss" scoped>
 @use '@/styles/components/avatar';
+
+.avatars {
+  display: inline-flex;
+  flex-direction: row-reverse;
+}
+
+.avatar {
+  position: relative;
+}
+
+.avatar:not(:last-child) {
+  margin-left: -15px;
+}
 </style>
