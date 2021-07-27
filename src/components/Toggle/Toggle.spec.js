c4d1b6f7a72c7a8d4b74fee3eb5209d928ed0722
Allyson Lubimir <allyson@prefect.io>
2021-07-27 10:41:09 -0400

    update tests

--- src/components/Toggle/Toggle.spec.js
+++ src/components/Toggle/Toggle.spec.js
@@ -1,79 +1,75 @@
 import { mount } from '@vue/test-utils'
-import RadioButton from './RadioButton.vue'
+import ToggleSwitch from './ToggleSwitch.vue'
 
 test('displays text in the default slot', () => {
   const text = 'Hello!'
-  const wrapper = mount(RadioButton, {
+  const wrapper = mount(ToggleSwitch, {
     slots: {
       default: text
     }
   })
 
-  const radio = wrapper.get('[data-test="default"]')
+  const toggle = wrapper.get('[data-test="default"]')
 
-  expect(radio.text()).toBe(text)
+  expect(toggle.text()).toBe(text)
 })
 
 describe('disabled state', () => {
   test('adds the disabled class when disabled:true is passed as a prop', () => {
-    const wrapper = mount(RadioButton, {
+    const wrapper = mount(ToggleSwitch, {
       props: {
         disabled: true
       }
     })
 
-    const radio = wrapper.get('label')
+    const toggle = wrapper.get('label')
 
-    expect(radio.classes()).toContain('disabled')
+    expect(toggle.classes()).toContain('disabled')
   })
 
   test('adds the disabled attribute when disabled:true is passed as a prop', () => {
-    const wrapper = mount(RadioButton, {
+    const wrapper = mount(ToggleSwitch, {
       props: {
         disabled: true
       }
     })
 
-    const radio = wrapper.get('label')
+    const toggle = wrapper.get('label')
 
-    expect(radio.attributes('disabled')).toBeDefined()
+    expect(toggle.attributes('disabled')).toBeDefined()
   })
 
   test('sets disabled to false when disabled:false is passed as a prop', () => {
-    const wrapper = mount(RadioButton, {
-      props: {
-        disabled: false
-      }
-    })
+    const wrapper = mount(ToggleSwitch)
 
-    const radio = wrapper.get('label')
+    const toggle = wrapper.get('label')
 
-    expect(radio.attributes('disabled')).toContain('false')
+    expect(toggle.attributes('disabled')).toContain('false')
   })
 })
 
 test('emit update to true when selected', async () => {
-  const wrapper = mount(RadioButton, {
-    props: {
-      disabled: false,
-      modelValue: false
-    }
-  })
-  const input = wrapper.find('input[type="radio"]')
-  input.element.checked = true
-  await input.trigger('input')
+  const wrapper = mount(ToggleSwitch)
+  const input = wrapper.get('input[type="checkbox"]')
+  await input.setValue(true)
   expect(wrapper.emitted('update:modelValue')[0][0]).toEqual(true)
 })
 
 test('emit update to false when unselected', async () => {
-  const wrapper = mount(RadioButton, {
+  const wrapper = mount(ToggleSwitch, {
     props: {
-      disabled: false,
-      modelValue: true
+      value: true
     }
   })
-  const input = wrapper.find('input[type="radio"]')
-  input.element.checked = false
-  await input.trigger('input')
+  const input = wrapper.get('input[type="checkbox"]')
+  await input.setValue(false)
   expect(wrapper.emitted('update:modelValue')[0][0]).toEqual(false)
+})
+
+test('updating prop also changes value', async () => {
+  // we have to test this way as vue-test-utils doesn't offer a good way to test "checked"
+  const wrapper = mount(ToggleSwitch)
+  await wrapper.setProps({ value: true })
+  const input = wrapper.get('input[type="checkbox"]')
+  expect(input.attributes('value')).toEqual('true')
 })
