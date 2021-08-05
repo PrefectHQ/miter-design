8fe373dd27d3720167d45750062a77e7a1587330
Jenny G <40272060+zhen0@users.noreply.github.com>
2021-08-05 16:21:29 -0400

    remove duplicate components

--- src/demo/App.vue
+++ src/demo/App.vue
@@ -1,13 +1,5 @@
 <template>
   <div class="pt-5 px-10 pb-10" v-cloak>
-    <Cards />
-    <Buttons class="mt-10" />
-    <Toggles class="mt-10" />
-    <RadioButtons class="mt-10" />
-    <Checkboxes />
-    <Links class="mt-10" />
-    <Sliders class="mt-10" />
-    <Tag class="mt-10" />
     <div>
       <router-link class="mr-4" to="/">Components</router-link>
       <router-link to="/icons">Icons</router-link>
