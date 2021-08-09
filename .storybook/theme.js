dda942065290927b0cb695789a17d9009ebc9838
nicholas <znicholasbrown@gmail.com>
2021-08-09 18:23:54 -0400

    Add some light theming

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,6 +1,7 @@
 import { create } from '@storybook/theming'
 
 export default create({
+  // appBg: 'linear-gradient(180deg, #0052ff 0%, #090422 100.05%)',
   base: 'light',
   brandTitle: 'Miter Design',
   brandUrl: 'https://github.com/PrefectHQ/miter-design',
