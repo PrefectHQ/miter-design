9ee893467b0d10cb85cae74c587f827ef5d5c74f
Natalie Smith <thatgalnatalie@gmail.com>
2021-08-16 13:57:41 -0700

    add multiple toggle to demo

--- src/demo/sections/TagGroup.vue
+++ src/demo/sections/TagGroup.vue
@@ -2,8 +2,9 @@
   <div>
     <h3>Tag Group</h3>
     v-model:{{ tags }}
+    <button @click="multiple = !multiple">multiple: {{ multiple }}</button>
 
-    <TagGroup v-model="tags" :multiple="true">
+    <TagGroup v-model="tags" :multiple="multiple">
       <Tag value="1" :color="'primary-101'">One</Tag>
       <Tag value="2" :color="'primary-100'">Two</Tag>
     </TagGroup>
@@ -15,6 +16,7 @@ import { Vue, Options } from 'vue-class-component'
 @Options({})
 export default class TagGroup extends Vue {
   tags = []
+  multiple = false
   mounted(): void {
     return
   }
