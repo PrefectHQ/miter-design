85834bf0911e9c9e21ed1405bd06827607bc3138
Natalie Smith <thatgalnatalie@gmail.com>
2021-08-24 11:01:15 -0700

    add mandatory prop and tests

--- src/demo/sections/TagGroup.vue
+++ src/demo/sections/TagGroup.vue
@@ -3,8 +3,9 @@
     <h3>Tag Group</h3>
     v-model:{{ tags }}
     <button @click="multiple = !multiple">multiple: {{ multiple }}</button>
+    <button @click="mandatory = !mandatory">mandatory: {{ mandatory }}</button>
 
-    <TagGroup v-model="tags" :multiple="multiple">
+    <TagGroup v-model="tags" :multiple="multiple" :mandatory="mandatory">
       <Tag value="1" :color="'primary-101'">One</Tag>
       <Tag value="2" :color="'primary-100'">Two</Tag>
     </TagGroup>
@@ -15,8 +16,9 @@
 import { Vue, Options } from 'vue-class-component'
 @Options({})
 export default class TagGroup extends Vue {
-  tags = ['1']
+  tags = []
   multiple = false
+  mandatory = false
   mounted(): void {
     return
   }
