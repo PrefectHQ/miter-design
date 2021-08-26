00c3a849c69235efa4c3967a097802659ae56e2f
Natalie Smith <thatgalnatalie@gmail.com>
2021-08-25 18:44:43 -0700

    add toggle buttons in demo section

--- src/demo/sections/Tooltip.vue
+++ src/demo/sections/Tooltip.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
+    <button @click="right = !right">right: {{ right }}</button>
+    <button @click="left = !left">left: {{ left }}</button>
+    <button @click="bottom = !bottom">bottom: {{ bottom }}</button>
+
     <div class="center">
-      <Tooltip text="Tooltip" :bottom="true">
+      <Tooltip text="Tooltip" :right="right" :left="left" :bottom="bottom">
         <!-- <button>Hover Over Me!</button> -->
         <i class="pi pi-Info-Question-Mark pi-3x" />
       </Tooltip>
@@ -9,8 +13,22 @@
   </div>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      right: false,
+      left: false,
+      bottom: false
+    }
+  }
+})
+</script>
 <style>
 .center {
   margin-left: 100px;
+  margin-top: 50px;
 }
 </style>
