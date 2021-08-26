51280264ddf7e8c84cfff60af9a514916abdc374
Natalie Smith <thatgalnatalie@gmail.com>
2021-08-25 17:19:07 -0700

    position top, bottom, left, right

--- src/demo/sections/Tooltip.vue
+++ src/demo/sections/Tooltip.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
-    <Tooltip text="Tooltip">
-      <button>Hover Over Me!</button>
-      <!-- <i class="pi pi-Info-Question-Mark pi-2x" /> -->
-    </Tooltip>
+    <div class="center">
+      <Tooltip text="Tooltip">
+        <!-- <button>Hover Over Me!</button> -->
+        <i class="pi pi-Info-Question-Mark pi-2x" />
+      </Tooltip>
+    </div>
   </div>
 </template>
+
+<style>
+.center {
+  margin-left: 100px;
+}
+</style>
