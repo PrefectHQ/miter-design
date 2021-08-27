f0ee86397edcb3e4e847f4aab0eaadc5257541d6
nicholas <znicholasbrown@gmail.com>
2021-08-27 16:35:43 -0400

    Remove commented code in toast container

--- src/components/Toast/ToastContainer.vue
+++ src/components/Toast/ToastContainer.vue
@@ -6,49 +6,11 @@
 
 <script lang="ts">
 import { Vue, Options } from 'vue-class-component'
-import { ref, defineAsyncComponent, defineComponent } from 'vue'
+import { ref } from 'vue'
 
-// @Options({
-//   components: {
-//     Toast: defineAsyncComponent(() => import('@/components/Toast/Toast.vue'))
-//   }
-// })
-
-// export default defineComponent({
-//   components: {
-//     Toast: defineAsyncComponent(() => import('@/components/Toast/Toast.vue'))
-//   },
-//   data() {
-//     return {
-//       toasts: [] as { id: string; text: string }[]
-//     }
-//   },
-//   methods: {
-//     functionaddToast(toast: { id: string; text: string }) {
-//       this.toasts.push(toast)
-//     },
-//     removeToast(id: string) {
-//       this.toasts.splice(
-//         this.toasts.findIndex((t) => t.id == id),
-//         1
-//       )
-//     }
-//   }
-// })
+@Options({})
 export default class ToastContainer extends Vue {
   container = ref<HTMLElement>() as unknown as HTMLElement
-  // const toasts: { id: string; text: string }[] = []
-
-  //  functionaddToast(toast: { id: string; text: string }) {
-  //   this.toasts.push(toast)
-  // }
-
-  //  removeToast(id: string) {
-  //   this.toasts.splice(
-  //     this.toasts.findIndex((t) => t.id == id),
-  //     1
-  //   )
-  // }
 }
 </script>
 
