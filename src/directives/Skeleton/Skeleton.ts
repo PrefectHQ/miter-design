f0699a0c84ad674f895fd91aed7f89570d30d123
nicholas <znicholasbrown@gmail.com>
2021-08-31 13:44:30 -0400

    Update skeleton directive with better class bindings

--- src/directives/Skeleton/Skeleton.ts
+++ src/directives/Skeleton/Skeleton.ts
@@ -1,6 +1,7 @@
 // Is this the correct way to apply this style? I'm trying to avoid a global import since that seems inappropriate
 import '@/styles/components/skeleton-loader.scss'
 import { BindingMetadata } from '@vue/compiler-core'
+import { DirectiveBinding, ObjectDirective, VNode } from '@vue/runtime-dom'
 
 // Using any bindings for these elements so typescript doesn't throw errors when we attempt to set disabled on non-input elements
 const applySkeletonClass = (el: any) => {
@@ -13,17 +14,19 @@ const removeSkeletonClass = (el: any) => {
   el.disabled = false
 }
 
-export default {
-  mounted(el: Element, binding: BindingMetadata) {
+const SkeletonDirective: ObjectDirective = {
+  mounted(el: any, binding: DirectiveBinding, vNode: VNode) {
     if (binding.value) applySkeletonClass(el)
   },
-  updated(el: Element, binding: BindingMetadata) {
+  updated(el: any, binding: DirectiveBinding, vNode: VNode) {
     // If the component updates and we haven't already applied/removed the
     // skeleton effect, apply or remove it now
     if (binding.value && !binding.oldValue) applySkeletonClass(el)
     if (!binding.value && binding.oldValue) removeSkeletonClass(el)
   },
-  unmounted(el: Element) {
+  unmounted(el: any, binding: DirectiveBinding, vNode: VNode) {
     removeSkeletonClass(el)
   }
 }
+
+export default SkeletonDirective
