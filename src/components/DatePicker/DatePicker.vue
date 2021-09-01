b0833412f518a3a2feb376e1aa4ba60cfd655309
nicholas <znicholasbrown@gmail.com>
2021-09-01 16:15:09 -0400

    Add basic logic for incrementing and decrementing month

--- src/components/DatePicker/DatePicker.vue
+++ src/components/DatePicker/DatePicker.vue
@@ -1,5 +1,16 @@
 <template>
-  <div> I'm a date picker! </div>
+  <div>
+    <div> Date: {{ date }} </div>
+    <div> Today: {{ today }} </div>
+    <div> Display Month: {{ displayMonth }} </div>
+    <div>
+      <span class="mr-2 cursor-pointer pa-2" @click="decrementMonth">-</span>
+      Month: {{ month }}
+      <span class="ml-2 cursor-pointer pa-2" @click="incrementMonth">+</span>
+    </div>
+    <div> Days in Month: {{ daysInMonth }} </div>
+    <div> Year: {{ year }} </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,7 +20,51 @@ class Props {}
 
 const Component = Options
 @Component({})
-export default class DatePicker extends Vue.with(Props) {}
+export default class DatePicker extends Vue.with(Props) {
+  date = new Date()
+
+  get today(): Date {
+    return new Date()
+  }
+
+  get month(): number {
+    return new Date(this.date).getMonth()
+  }
+
+  get displayMonth(): string {
+    const months: string[] = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ]
+    return months[this.month]
+  }
+
+  get year(): number {
+    return new Date(this.date).getFullYear()
+  }
+
+  get daysInMonth(): number {
+    return 32 - new Date(this.year, this.month, 32).getDate()
+  }
+
+  incrementMonth() {
+    this.date = new Date(this.year, this.month + 1)
+  }
+
+  decrementMonth() {
+    this.date = new Date(this.year, this.month - 1)
+  }
+}
 </script>
 
 <style lang="scss" scoped>
