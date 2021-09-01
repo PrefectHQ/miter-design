e333c5f890b8df5ad37c7a50df406dfdf9581e43
nicholas <znicholasbrown@gmail.com>
2021-08-31 20:03:48 -0400

    Add style mocks and tests for skeleton directive

--- src/directives/Skeleton/Skeleton.ts
+++ src/directives/Skeleton/Skeleton.ts
@@ -1,30 +1,31 @@
-// Is this the correct way to apply this style? I'm trying to avoid a global import since that seems inappropriate
 import '@/styles/components/skeleton-loader.scss'
 import { DirectiveBinding, ObjectDirective, VNode } from '@vue/runtime-dom'
 
+const skeletonClass = 'skeleton-loader'
+
 // Using any bindings for these elements so typescript doesn't throw errors when we attempt to set disabled on non-input elements
-const applySkeletonClass = (el: any) => {
-  el.classList.add('skeleton-loader')
+export const applyClass = (el: any) => {
+  el.classList.add(skeletonClass)
   el.disabled = true
 }
 
-const removeSkeletonClass = (el: any) => {
-  el.classList.remove('skeleton-loader')
+export const removeClass = (el: any) => {
+  el.classList.remove(skeletonClass)
   el.disabled = false
 }
 
-const SkeletonDirective: ObjectDirective = {
+export const SkeletonDirective: ObjectDirective = {
   mounted(el: any, binding: DirectiveBinding, vNode: VNode) {
-    if (binding.value) applySkeletonClass(el)
+    if (binding.value) applyClass(el)
   },
   updated(el: any, binding: DirectiveBinding, vNode: VNode) {
     // If the component updates and we haven't already applied/removed the
     // skeleton effect, apply or remove it now
-    if (binding.value && !binding.oldValue) applySkeletonClass(el)
-    if (!binding.value && binding.oldValue) removeSkeletonClass(el)
+    if (binding.value && !binding.oldValue) applyClass(el)
+    else if (!binding.value && binding.oldValue) removeClass(el)
   },
   unmounted(el: any, binding: DirectiveBinding, vNode: VNode) {
-    removeSkeletonClass(el)
+    removeClass(el)
   }
 }
 
