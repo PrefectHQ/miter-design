d32dbb34ab179574386a460743d9bbba8f3ebc2d
Jennifer Grange <jenny@prefect.io>
2021-09-08 17:04:41 -0400

    format tests

--- src/components/Input/Input.spec.js
+++ src/components/Input/Input.spec.js
@@ -3,97 +3,98 @@ import Input from './Input.vue'
 
 describe('Slots', () => {
   test('displays icon in the prepend slot', () => {
-  const wrapper = mount(Input, {
-    slots: {
-      prepend: `<i class="pi pi-Search pi-2x"></i>`
-    }
+    const wrapper = mount(Input, {
+      slots: {
+        prepend: `<i class="pi pi-Search pi-2x"></i>`
+      }
+    })
+
+    const icon = wrapper.get('[data-test="prepend"]')
+    expect(icon.html()).toBe(
+      `<span class="prepend" data-test="prepend"><i class="pi pi-Search pi-2x"></i></span>`
+    )
   })
 
-  const icon = wrapper.get('[data-test="prepend"]')
-  expect(icon.html()).toBe(`<span class="prepend" data-test="prepend"><i class="pi pi-Search pi-2x"></i></span>`)
-})
+  test('displays text in the append slot', () => {
+    const wrapper = mount(Input, {
+      slots: {
+        append: `<span>Info</span>`
+      }
+    })
 
-test('displays text in the append slot', () => {
-  const wrapper = mount(Input, {
-    slots: {
-      append: `<span>Info</span>`
-    }
+    const append = wrapper.get('[data-test="append"]')
+    expect(append.text()).toBe(`Info`)
   })
-
-  const append = wrapper.get('[data-test="append"]')
-  expect(append.text()).toBe(`Info`)
-})
 })
 
 describe('Props', () => {
   test('displays label', () => {
-  const wrapper = mount(Input, {
-    props: {
-      label: 'Default'
-    }
+    const wrapper = mount(Input, {
+      props: {
+        label: 'Default'
+      }
+    })
+    const label = wrapper.get('[data-test="label"]')
+    expect(label.text()).toBe('Default')
   })
-  const label = wrapper.get('[data-test="label"]')
-  expect(label.text()).toBe('Default')
-})
 
-test('displays subtitle', () => {
-  const wrapper = mount(Input, {
-    props: {
-      subtitle: 'Sub'
-    }
+  test('displays subtitle', () => {
+    const wrapper = mount(Input, {
+      props: {
+        subtitle: 'Sub'
+      }
+    })
+    const subtitle = wrapper.get('[data-test="subtitle"]')
+    expect(subtitle.text()).toBe('Sub')
   })
-  const subtitle = wrapper.get('[data-test="subtitle"]')
-  expect(subtitle.text()).toBe('Sub')
-})
 
-test('passes valid state', () => {
-  const wrapper = mount(Input, {
-    props: {
-      valid: false
-    }
+  test('passes valid state', () => {
+    const wrapper = mount(Input, {
+      props: {
+        valid: false
+      }
+    })
+    const input = wrapper.get('[data-test="default"]')
+    expect(input.attributes('valid')).toBe('false')
   })
-  const input = wrapper.get('[data-test="default"]')
-  expect(input.attributes('valid')).toBe("false")
-})
 
-test('passes maxLength', () => {
-  const wrapper = mount(Input, {
-    props: {
-      maxLength: 5,
-      modelValue: 'Test'
-    }
+  test('passes maxLength', () => {
+    const wrapper = mount(Input, {
+      props: {
+        maxLength: 5,
+        modelValue: 'Test'
+      }
+    })
+    const input = wrapper.get('[data-test="default"]')
+    expect(input.attributes('maxlength')).toBe('5')
+    expect(input.attributes('valid')).toBe('true')
   })
-  const input = wrapper.get('[data-test="default"]')
-  expect(input.attributes('maxlength')).toBe("5")
-  expect(input.attributes('valid')).toBe('true')
-})
 
-test('passes minlength', () => {
-  const wrapper = mount(Input, {
-    props: {
-      minLength: 2,
-      modelValue: 'Test'
-    }
+  test('passes minlength', () => {
+    const wrapper = mount(Input, {
+      props: {
+        minLength: 2,
+        modelValue: 'Test'
+      }
+    })
+    const input = wrapper.get('[data-test="default"]')
+    expect(input.attributes('minlength')).toBe('2')
   })
-  const input = wrapper.get('[data-test="default"]')
-  expect(input.attributes('minlength')).toBe("2")
-})
 
-test('passes pattern', () => {
-  const wrapper = mount(Input, {
-    props: {
-      pattern: '/',
-      modelValue: 'Test'
-    }
+  test('passes pattern', () => {
+    const wrapper = mount(Input, {
+      props: {
+        pattern: '/',
+        modelValue: 'Test'
+      }
+    })
+    const input = wrapper.get('[data-test="default"]')
+    expect(input.attributes('pattern')).toBe('/')
   })
-  const input = wrapper.get('[data-test="default"]')
-  expect(input.attributes('pattern')).toBe('/')
-})
-
 })
 
 describe('Emits Input', () => {
-  test('emits value', async() => {
+  test('emits value', async () => {
     const wrapper = mount(Input, {
       props: {
         modelValue: 'At'
@@ -108,8 +109,9 @@ describe('Emits Input', () => {
 describe('active state', () => {
   const wrapper = mount(Input, {
     props: {
-    modelValue: 'Input'
-  }})
+      modelValue: 'Input'
+    }
+  })
   const input = wrapper.get('input')
 
   test('mousedown adds the active class', async () => {
@@ -136,27 +138,27 @@ describe('active state', () => {
 })
 
 describe('disabled state', () => {
-    test('adds the disabled class when disabled:true is passed as a prop', () => {
-      const wrapper = mount(Input, {
-        props: {
-          disabled: true,
-          modelValue: '3'
-        }
-      })
-      const inputElement = wrapper.get('input')
-      expect(inputElement.classes()).toContain('disabled')
+  test('adds the disabled class when disabled:true is passed as a prop', () => {
+    const wrapper = mount(Input, {
+      props: {
+        disabled: true,
+        modelValue: '3'
+      }
     })
-  
-    test('adds the disabled attribute when disabled:true is passed as a prop', () => {
-      const wrapper = mount(Input, {
-        props: {
-          disabled: true,
-          modelValue: '3'
-        }
-      })
-      const inputElement = wrapper.get('input')
-      expect(inputElement.attributes('disabled')).toBeDefined()
+    const inputElement = wrapper.get('input')
+    expect(inputElement.classes()).toContain('disabled')
+  })
+
+  test('adds the disabled attribute when disabled:true is passed as a prop', () => {
+    const wrapper = mount(Input, {
+      props: {
+        disabled: true,
+        modelValue: '3'
+      }
     })
+    const inputElement = wrapper.get('input')
+    expect(inputElement.attributes('disabled')).toBeDefined()
+  })
 })
 
 describe('error state', () => {
@@ -170,17 +172,17 @@ describe('error state', () => {
     const inputElement = wrapper.get('input')
     expect(inputElement.classes()).toContain('invalid')
   })
-  
-    test('emits invalid', async() => {
-      const wrapper = mount(Input, {
-        props: {
-          modelValue: 'At',
-          required: true
-        }
-      })
-      const input = wrapper.get('input')
-      await input.setValue('')
-      expect(input.classes()).toContain('invalid')
-      expect(wrapper.emitted('invalid')[0][0]).toBeTruthy()
+
+  test('emits invalid', async () => {
+    const wrapper = mount(Input, {
+      props: {
+        modelValue: 'At',
+        required: true
+      }
     })
+    const input = wrapper.get('input')
+    await input.setValue('')
+    expect(input.classes()).toContain('invalid')
+    expect(wrapper.emitted('invalid')[0][0]).toBeTruthy()
+  })
 })
