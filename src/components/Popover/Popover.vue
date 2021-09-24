f285e60757ef5ea34ebb58b52f20fe47b2dc5021
Jenny G <40272060+zhen0@users.noreply.github.com>
2021-09-24 11:46:43 -0400

    Update src/components/Popover/Popover.vue
    Co-authored-by: Nicholas Brown <nicholas@prefect.io>

--- src/components/Popover/Popover.vue
+++ src/components/Popover/Popover.vue
@@ -108,7 +108,7 @@ export default defineComponent({
     }
   },
   methods: {
-    setPreviouslyFocussed() {
+    setPreviouslyFocused() {
       if (typeof document !== 'undefined')
         this.previouslyFocused =
           document.activeElement === document.body ||
