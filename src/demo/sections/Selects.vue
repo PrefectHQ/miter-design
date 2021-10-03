9d223d13c02dd464ff6a59019b5ccda216f6ec7f
nicholas <znicholasbrown@gmail.com>
2021-10-03 19:07:09 -0400

    Make sure open select menus are always above other content

--- src/demo/sections/Selects.vue
+++ src/demo/sections/Selects.vue
@@ -15,7 +15,6 @@
         name="with-icon"
         placeholder="This one has props!"
         search
-        openUp
         :options="options"
         v-model="selected2"
       >
@@ -48,6 +47,60 @@
         class="my-2"
         name="disabled"
         placeholder="You cannot choose here"
+        disabled
+      >
+      </Select>
+    </div>
+    <div class="d-flex">
+      <Select
+        class="my-2 mr-2"
+        name="without-icon"
+        :options="options"
+        open-up
+        v-model="selected1"
+      >
+        <Option v-for="(option, i) in options" :key="i" :value="option" />
+      </Select>
+
+      <Select
+        class="my-2 mr-2"
+        name="with-icon"
+        placeholder="This one has props!"
+        search
+        open-up
+        :options="options"
+        v-model="selected2"
+      >
+        <OptionGroup label="V-For Options">
+          <Option
+            v-for="(option, i) in options"
+            :key="i"
+            :value="option"
+            icon="fire-line"
+          />
+        </OptionGroup>
+        <OptionGroup label="Object Options">
+          <Option
+            v-for="(option, i) in items"
+            :key="i"
+            :data="option"
+            :value="option.label"
+            icon="fire-line"
+          ></Option>
+        </OptionGroup>
+        <OptionGroup label="Single Options">
+          <Option value="1" /><Option value="2" /><Option value="3" />
+        </OptionGroup>
+        <OptionGroup label="Disabled Group" disabled>
+          <Option value="no click" />
+        </OptionGroup>
+        <Option value="4" /><Option value="5" disabled />
+      </Select>
+      <Select
+        class="my-2"
+        name="disabled"
+        placeholder="You cannot choose here"
+        open-up
         disabled
       >
       </Select>
