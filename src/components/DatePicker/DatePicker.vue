451bc78b295f348796cec0971b524f7e8dd5491d
Jennifer Grange <jenny@prefect.io>
2021-10-04 16:11:53 -0400

    stop border removal on focus

--- src/components/DatePicker/DatePicker.vue
+++ src/components/DatePicker/DatePicker.vue
@@ -1,8 +1,8 @@
 <template>
   <div
     class="calendar"
-    @keyup.right.prevent="() => incrementMonth(true)"
-    @keyup.left.prevent="() => decrementMonth(true)"
+    @keyup.right.stop="incrementMonth(true)"
+    @keyup.left.stop="decrementMonth(true)"
     tabindex="0"
     ref="calendar"
   >
