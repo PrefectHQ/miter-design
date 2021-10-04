4d703ee5eddbe784d80ce2a325035b171554c803
Jennifer Grange <jenny@prefect.io>
2021-10-04 14:39:01 -0400

    add basic story for datepicker

---
+++ src/components/DatePicker/DatePicker.stories.js
@@ -0,0 +1,28 @@
+import DatePicker from './Datepicker.vue'
+import { ref } from 'vue'
+
+export default {
+  title: 'Miter Design/Date Picker',
+  component: DatePicker,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/JomY9jsgJdal9wfPBtpWkY/UI?node-id=1011%3A26505'
+    }
+  }
+}
+
+const Template = (args) => ({
+  components: { DatePicker },
+  setup() {
+    const modelValue = ref(args.modelValue)
+    return { args }
+  },
+  template: `<DatePicker v-bind="args"></DatePicker>`
+})
+
+export const Default = Template.bind({})
+Default.args = {
+    modelValue: new Date()
+}
+
