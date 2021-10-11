65aae32af27e622c7a56188867b4b362ae38a22b
Craig Harshbarger <pleek91@gmail.com>
2021-10-11 13:50:25 -0500

    Update dom utilities to account for tabIndex changes

--- src/utilities/dom.ts
+++ src/utilities/dom.ts
@@ -1,20 +1,22 @@
 export function getTabbableElements(element: HTMLElement) {
-    const elements = element.querySelectorAll<HTMLElement>('*')
-    const tabbable = [...elements].filter(element => element.tabIndex >= 0)
+    const elements = [...element.querySelectorAll<HTMLElement>('*')]
+    const tabbable = elements.filter(element => element.tabIndex >= 0)
 
     return tabbable
 }
 
 export function getFirstTabbableElement(element: HTMLElement) {
-    const elements = element.querySelectorAll<HTMLElement>('*')
-    const tabbable = [...elements].find(element => element.tabIndex >= 0)
+    const elements = getTabbableElements(element);
+    const sorted = elements.sort((a, b) => a.tabIndex - b.tabIndex)
+    const first = sorted[0]
 
-    return tabbable
+    return first
 }
 
 export function getLastTabbableElement(element: HTMLElement) {
-    const elements = element.querySelectorAll<HTMLElement>('*')
-    const tabbable = [...elements].reverse().find(element => element.tabIndex >= 0)
+    const elements = getTabbableElements(element);
+    const sorted = elements.sort((a, b) => a.tabIndex - b.tabIndex)
+    const last = sorted[sorted.length - 1]
 
-    return tabbable
+    return last
 }
