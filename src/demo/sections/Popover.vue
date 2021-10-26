2b9cff80228c4615742c0849164d18387a2b6c3d
Craig Harshbarger <pleek91@gmail.com>
2021-10-26 09:45:03 -0500

    Make sure the target is the correct size

--- src/demo/sections/Popover.vue
+++ src/demo/sections/Popover.vue
@@ -1,43 +1,45 @@
 <template>
   <div>
     <h3 class="mb-2">Popover</h3>
-    <Popover position="right">
-      <template v-slot:header>
-        <p class="ma-0"><i class="pi pi-user-smile-line pi-lg"></i> Flow Run Duration</p>
-      </template>
-      <template v-slot:trigger="{ toggle, open }">
-        <Button @keydown.enter="toggle" @mouseover="open">
-          Popover
-        </Button>
-      </template>
-      <template v-slot="{ close }">
-        <div>
-          <div class="d-flex justify-space-between">
-            <span>Date:</span><span>Aug 1, 2021</span>
+    <div style="display: flex;">
+      <Popover position="right">
+        <template v-slot:header>
+          <p class="ma-0"><i class="pi pi-user-smile-line pi-lg"></i> Flow Run Duration</p>
+        </template>
+        <template v-slot:trigger="{ toggle, open }">
+          <Button @keydown.enter="toggle" @mouseover="open">
+            Popover
+          </Button>
+        </template>
+        <template v-slot="{ close }">
+          <div>
+            <div class="d-flex justify-space-between">
+              <span>Date:</span><span>Aug 1, 2021</span>
+            </div>
+            <div class="d-flex justify-space-between">
+              <span>Run Range:</span><span>1-58m</span>
+            </div>
+            <div class="d-flex justify-space-between">
+              <span>Flow Runs:</span><span>20</span>
+            </div>
+            <hr />
+            <div class="d-flex justify-space-between">
+              <span>Average Latness:</span><span>3m</span>
+            </div>
+            <div class="d-flex justify-space-between">
+              <span>Highest Range:</span><span>1-59m</span>
+            </div>
+            <div class="d-flex justify-space-between">
+              <span>Lowest Range:</span><span>&gt;1d</span>
+            </div>
           </div>
-          <div class="d-flex justify-space-between">
-            <span>Run Range:</span><span>1-58m</span>
+          <div class="text-center mt-4">
+            <Button class="mr-1" color="primary" @click="handleClick">Click</Button>
+            <Button color="primary" @click="close">Close</Button>
           </div>
-          <div class="d-flex justify-space-between">
-            <span>Flow Runs:</span><span>20</span>
-          </div>
-          <hr />
-          <div class="d-flex justify-space-between">
-            <span>Average Latness:</span><span>3m</span>
-          </div>
-          <div class="d-flex justify-space-between">
-            <span>Highest Range:</span><span>1-59m</span>
-          </div>
-          <div class="d-flex justify-space-between">
-            <span>Lowest Range:</span><span>&gt;1d</span>
-          </div>
-        </div>
-        <div class="text-center mt-4">
-          <Button class="mr-1" color="primary" @click="handleClick">Click</Button>
-          <Button color="primary" @click="close">Close</Button>
-        </div>
-      </template>
-    </Popover>
+        </template>
+      </Popover>
+    </div>
   </div>
 </template>
 
