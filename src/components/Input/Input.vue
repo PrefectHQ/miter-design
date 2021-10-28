624e4453b8390381eb4f95a0bde83c71ca84f922
Craig Harshbarger <pleek91@gmail.com>
2021-10-28 14:31:18 -0500

    Make sure Input emits a blur event

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -136,7 +136,7 @@ export default defineComponent({
       required: false
     }
   },
-  emits: ['update:modelValue', 'invalid', 'keyup', 'keypress', 'keydown'],
+  emits: ['update:modelValue', 'invalid', 'keyup', 'keypress', 'keydown', 'blur'],
   data() {
     return {
       active: false as boolean,
@@ -205,6 +205,8 @@ export default defineComponent({
       this.validate(e)
       this.hovered = false
       this.active = false
+
+      this.$emit('blur', e)
     },
     validate(e: Event) {
       const valid = e.target?.checkValidity()
