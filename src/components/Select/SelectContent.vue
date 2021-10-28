59606a869c64097bec39c33f3defca85c8dcd102
Craig Harshbarger <pleek91@gmail.com>
2021-10-28 17:11:07 -0500

    Make sure SelectContent scrolls within itself when using miter mixins

--- src/components/Select/SelectContent.vue
+++ src/components/Select/SelectContent.vue
@@ -1,32 +1,34 @@
 <template>
     <div class="select-content" tabindex="0" @keydown="keydown" @focusout="focusout">
-      <slot name="before-options" />
-      <div class="select-content__options">
-        <template v-for="option in options" :key="option.label">
-          <template v-if="isOptionGroup(option)">
-            <OptionGroup v-bind="option" :selected="selected" @select="select">
-              <template v-slot:label="scope">
-                <slot name="option-group-label" v-bind="scope" />
-              </template>
-              <template v-slot:option-icon="scope">
-                <slot name="option-icon" v-bind="scope" />
-              </template>
-              <template v-slot:option-label="scope">
-                <slot name="option-label" v-bind="scope" />
-              </template>
-            </OptionGroup>
+      <div class="select-content__container">
+        <slot name="before-options" />
+        <div class="select-content__options">
+          <template v-for="option in options" :key="option.label">
+            <template v-if="isOptionGroup(option)">
+              <OptionGroup v-bind="option" :selected="selected" @select="select">
+                <template v-slot:label="scope">
+                  <slot name="option-group-label" v-bind="scope" />
+                </template>
+                <template v-slot:option-icon="scope">
+                  <slot name="option-icon" v-bind="scope" />
+                </template>
+                <template v-slot:option-label="scope">
+                  <slot name="option-label" v-bind="scope" />
+                </template>
+              </OptionGroup>
+            </template>
+            <template v-else>
+              <Option v-bind="option" :selected="isOptionSelected(option, selected)" @select="select">
+                <template v-slot:icon="scope">
+                  <slot name="option-icon" v-bind="scope" />
+                </template>
+                <template v-slot:label="scope">
+                  <slot name="option-label" v-bind="scope" />
+                </template>
+              </Option>
+            </template>
           </template>
-          <template v-else>
-            <Option v-bind="option" :selected="isOptionSelected(option, selected)" @select="select">
-              <template v-slot:icon="scope">
-                <slot name="option-icon" v-bind="scope" />
-              </template>
-              <template v-slot:label="scope">
-                <slot name="option-label" v-bind="scope" />
-              </template>
-            </Option>
-          </template>
-        </template>
+        </div>
       </div>
     </div>
 </template>
@@ -72,15 +74,14 @@ export default class SelectContent extends Vue.with(Props) {
 </script>
 
 <style lang="scss">
-@use '../../styles/abstracts/mixins';
 
 .select-content {
   width: var(--select-width, 350px);
-  max-height: calc(var(--select-height, 58px) * 6);
   background-color: #fff;
-  overflow-y: auto;
-  z-index: 1;
+}
 
-  @include mixins.drop-shadow;
+.select-content__container {
+  max-height: calc(var(--select-height, 58px) * 6);
+  overflow-y: auto;
 }
 </style>
