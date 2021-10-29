14f84cab505ba5e7fa7838146b3d7ea63fd116f9
Craig Harshbarger <pleek91@gmail.com>
2021-10-29 11:30:44 -0500

    Add all the components to the index installer

--- src/components/Select/index.ts
+++ src/components/Select/index.ts
@@ -1,13 +1,17 @@
 import Select from './Select.vue'
 import Option from './Option.vue'
+import OptionLabel from './OptionLabel.vue'
 import OptionGroup from './OptionGroup.vue'
+import SelectContent from './SelectContent.vue'
 
 import { App } from 'vue'
 
 Select.install = (app: App) => {
-  app.component(Select.name, Select)
-  app.component(Option.name, Option)
-  app.component(OptionGroup.name, OptionGroup)
+  app.component('Select', Select)
+  app.component('Option', Option)
+  app.component('OptionLabel', OptionLabel)
+  app.component('SelectContent', SelectContent)
+  app.component('OptionGroup', OptionGroup)
 }
 
 export default Select
