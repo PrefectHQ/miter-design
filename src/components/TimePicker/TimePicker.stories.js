36b404d6d63c63d0b41a16d181d2b0e6a45743bd
Craig Harshbarger <pleek91@gmail.com>
2021-11-01 14:54:09 -0500

    Fix imports and uses of TimePicker that were not case sensitive

--- src/components/TimePicker/TimePicker.stories.js
+++ src/components/TimePicker/TimePicker.stories.js
@@ -1,9 +1,9 @@
-import Timepicker from './Timepicker.vue'
+import TimePicker from './TimePicker.vue'
 import { ref } from 'vue'
 
 export default {
-  title: 'Miter Design/Timepicker',
-  component: Timepicker,
+  title: 'Miter Design/TimePicker',
+  component: TimePicker,
   parameters: {
     design: {
       type: 'figma',
@@ -14,14 +14,14 @@ export default {
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  components: { Timepicker },
+  components: { TimePicker },
   setup() {
     const modelValue = ref(args.modelValue)
     const disabled = ref(args.disabled || false)
 
     return { ...args, modelValue, disabled }
   },
-  template: '<Timepicker v-model="modelValue" :disabled="disabled" />'
+  template: '<TimePicker v-model="modelValue" :disabled="disabled" />'
 })
 
 export const Default = Template.bind({})
