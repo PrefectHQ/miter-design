50b9a3cabbbf6a0dac745400a21ae7f4150dd628
Craig Harshbarger <pleek91@gmail.com>
2021-11-01 09:37:48 -0500

    Add NativeSelect and SimpleSelect to the install

--- src/components/Select/index.ts
+++ src/components/Select/index.ts
@@ -1,4 +1,6 @@
 import Select from './Select.vue'
+import NativeSelect from './NativeSelect.vue'
+import SimpleSelect from './SimpleSelect.vue'
 import Option from './Option.vue'
 import OptionLabel from './OptionLabel.vue'
 import OptionGroup from './OptionGroup.vue'
@@ -8,6 +10,8 @@ import { App } from 'vue'
 
 Select.install = (app: App) => {
   app.component('Select', Select)
+  app.component('NativeSelect', NativeSelect)
+  app.component('SimpleSelect', SimpleSelect)
   app.component('Option', Option)
   app.component('OptionLabel', OptionLabel)
   app.component('SelectContent', SelectContent)
