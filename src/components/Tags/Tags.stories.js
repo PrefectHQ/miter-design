cc0be5f7135e317c0d968d97709adb3e0686bdb8
Marichka Offen <marichka@prefect.io>
2021-12-14 17:18:14 -0500

    not-working stories

---
+++ src/components/Tags/Tags.stories.js
@@ -0,0 +1,37 @@
+import Tags from './Tags.vue'
+
+export default {
+    title: 'Miter Design/Tags',
+    component: Tags,
+    args: {
+        content: 'Tags'
+    },
+    parameters: {
+        design: {
+            type: 'figma',
+            url: 'https://www.figma.com/file/HJgj2eHBaqt1SAYruZNjQQ/Prefect?node-id=1080%3A4411'
+        }
+    }
+}
+
+const Template = (args) => ({
+    components: { Tags },
+    setup() {
+        return { args }
+    },
+    template: '<Tags v-bind="args" />'
+})
+
+export const Default = Template.bind({})
+
+export const EmptyInternalTags = Template.bind({})
+EmptyInternalTags.args = {
+    tags: [],
+    icon: 'pi-label'
+}
+
+export const InternalTags = Template.bind({})
+InternalTags.args = {
+    tags: ["foo", "bar"],
+    icon: 'pi-label'
+}
