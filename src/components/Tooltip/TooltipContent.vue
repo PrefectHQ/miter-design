c6917264313ab1631642e1bbb586e3f67d140ba3
Craig Harshbarger <pleek91@gmail.com>
2021-12-16 14:41:07 -0600

    Fix ToolTip content not having the component decorator and breaking stuff

--- src/components/Tooltip/TooltipContent.vue
+++ src/components/Tooltip/TooltipContent.vue
@@ -11,12 +11,14 @@
 <script lang="ts">
 import { Placement } from '../../utilities/position'
 import { Vue, prop } from 'vue-class-component'
+import { Component } from '@/utilities/vue-class-component'
 
 class Props {
   placement = prop<Placement>({ default: 'top' })
   content = prop<string>({ required: false, default: '' })
 }
 
+@Component({})
 export default class TooltipContent extends Vue.with(Props) {
 
   get classes() {
