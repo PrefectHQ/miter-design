fdbf12837fc9ffcbebdfdd9c3beb276b553141b7
Natalie Smith <thatgalnatalie@gmail.com>
2021-12-29 19:52:55 -0800

    add tests for slots and props

--- src/components/DataTable/DataTable.spec.js
+++ src/components/DataTable/DataTable.spec.js
@@ -24,51 +24,108 @@ const rows = [
   { name: 'Winter Interns', memberCount: 90, roles: 'Admin' }
 ]
 
-test('displays columns', () => {
-  const wrapper = mount(DataTable, {
-    props: {
-      rows: rows,
-      columns: columns
-    }
+const ascRows = [
+  { name: 'Data Science', memberCount: 405, roles: 'Admin' },
+  { name: 'Dev Ops', memberCount: 22, roles: 'Admin' },
+  { name: 'Production Team', memberCount: 35, roles: 'Admin' },
+  { name: 'Staging Team', memberCount: 57, roles: 'Admin' },
+  { name: 'Winter Interns', memberCount: 90, roles: 'Admin' }
+]
+
+describe('props', () => {
+  test('displays columns', () => {
+    const wrapper = mount(DataTable, {
+      props: {
+        rows: rows,
+        columns: columns
+      }
+    })
+
+    const datatableHeader = wrapper.findAll('.data-table__table-header')
+    expect(datatableHeader[0].text()).toBe(columns[0].label)
+    expect(datatableHeader[1].text()).toBe(columns[1].label)
+    expect(datatableHeader[2].text()).toBe(columns[2].label)
+    expect(datatableHeader.length).toBe(columns.length)
   })
 
-  const datatableHeader = wrapper.findAll('.data-table__table-header')
-  expect(datatableHeader[0].text()).toBe(columns[0].label)
-  expect(datatableHeader[1].text()).toBe(columns[1].label)
-  expect(datatableHeader[2].text()).toBe(columns[2].label)
-  expect(datatableHeader.length).toBe(columns.length)
+  test('displays rows', () => {
+    const wrapper = mount(DataTable, {
+      props: {
+        rows: rows,
+        columns: columns
+      }
+    })
+
+    const datatableRows = wrapper.findAll('.data-table__table-row')
+    expect(datatableRows.length).toBe(rows.length)
+  })
 })
 
-test('displays rows', () => {
-  const wrapper = mount(DataTable, {
-    props: {
-      rows: rows,
-      columns: columns
-    }
+describe('emits', () => {
+  test('direction', async () => {
+    const wrapper = mount(DataTable, {
+      props: {
+        rows: rows,
+        columns: columns
+      }
+    })
+
+    const header = wrapper.find('.data-table__table-header')
+    await header.trigger('click')
+
+    expect(wrapper.emitted('update:direction')[0][0]).toEqual('asc')
+    expect(wrapper.vm.sorted).toEqual(ascRows)
   })
 
-  const datatableRows = wrapper.findAll('.data-table__table-row')
-  expect(datatableRows.length).toBe(rows.length)
+  test('sort by', async () => {
+    const wrapper = mount(DataTable, {
+      props: {
+        rows: rows,
+        columns: columns
+      }
+    })
+
+    const header = wrapper.find('.data-table__table-header')
+    await header.trigger('click')
+
+    expect(wrapper.emitted('update:sortBy')[0][0]).toEqual('name')
+  })
 })
 
-test('sort direction via click', () => {
-  const wrapper = mount(DataTable, {
-    props: {
-      rows: rows,
-      columns: columns
-    }
+describe('slots', () => {
+  test('columns', () => {
+    const wrapper = mount(DataTable, {
+      props: {
+        rows: rows,
+        columns: columns
+      },
+      slots: {
+        'column-header-member-count': `
+      <template #column-header-member-count="{ label }">
+        Total: {{ label }}
+      </template>
+      `
+      }
+    })
+
+    expect(wrapper.html()).toContain('Total: Member Count')
   })
 
-  const header = wrapper.findAll('.data-table__table-header')[0]
-  header.trigger('click')
+  test('rows', () => {
+    const wrapper = mount(DataTable, {
+      props: {
+        rows: rows,
+        columns: columns
+      },
+      slots: {
+        'column-member-count': `
+        <template #column-member-count="{ value }">
+          <span>Member Count: {{ value }}</span>
+        </template>
+      `
+      }
+    })
 
-  const ascRows = [
-    { name: 'Data Science', memberCount: 405, roles: 'Admin' },
-    { name: 'Dev Ops', memberCount: 22, roles: 'Admin' },
-    { name: 'Production Team', memberCount: 35, roles: 'Admin' },
-    { name: 'Staging Team', memberCount: 57, roles: 'Admin' },
-    { name: 'Winter Interns', memberCount: 90, roles: 'Admin' }
-  ]
-
-  expect(wrapper.vm.sorted).toEqual(ascRows)
+    expect(wrapper.html()).toContain('Member Count: 405')
+  })
 })
