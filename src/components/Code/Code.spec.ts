d181e48f067186ba4ad8043067b5d358e3a08b96
nicholas <znicholasbrown@gmail.com>
2022-02-10 15:29:42 -0500

    Add Code component tests

--- src/components/Code/Code.spec.ts
+++ src/components/Code/Code.spec.ts
@@ -9,7 +9,47 @@ test('displays text in the default slot', () => {
     }
   })
 
-  const code = wrapper.get('[data-test="default"]')
+  const code = wrapper.get('.code__content')
 
   expect(code.text()).toBe(text)
+})
+
+test('displays text in the file-name slot', () => {
+  const text = 'test_file.js'
+  const wrapper = mount(Code, {
+    slots: {
+      'file-name': text
+    }
+  })
+
+  const code = wrapper.get('.code__file-name')
+
+  expect(code.text()).toBe(text)
+})
+
+test('displays the copy button', () => {
+  const text = ''
+  const wrapper = mount(Code, {
+    slots: {
+      default: text
+    }
+  })
+
+
+  expect(wrapper.find('.code__copy-button').exists()).toBe(true)
+})
+
+test('does\'t display the copy button if hide-copy-button is passed', () => {
+  const text = ''
+  const wrapper = mount(Code, {
+    attrs: {
+      'hide-copy-button': true
+    },
+    slots: {
+      default: text
+    }
+  })
+
+
+  expect(wrapper.find('.code__copy-button').exists()).toBe(false)
 })
